8651a31dafc8e8a44f5d498a97e97f193e22c188
iammyeongho <jmh4912@naver.com>
2025-05-31 17:15:57 +0900

    [20240522] fix: 순환참조 방지를 위해 db/base.py에서 모델 import 제거

--- app/db/base.py
+++ app/db/base.py
@@ -1,8 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import MetaData
-from app.models.user import User
-from app.models.project import Project
-from app.models.monitoring import MonitoringLog, MonitoringAlert, MonitoringSetting
 
 # py_monitor 스키마를 사용하도록 MetaData 설정
 metadata = MetaData(schema="py_monitor")
